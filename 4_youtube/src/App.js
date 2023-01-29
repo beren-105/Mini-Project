4e37db12a775a77ea9604f980669a2747058d403
Beren <k33250@naver.com>
2023-01-29 18:37:16 +0900

    fliterBtn

---
+++ 4_youtube/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Header from './Header'
+
+const content = [
+  {id: 'watch-0', user: 'chster', category: '음악'},
+]
+
+const category = ['전체', '음악', '실시간', '게임', '뉴스', '축구', '요리']
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <FliterBtn 
+        category = {category}
+      />
+    </>
+  )
+}
+
+
+function FliterBtn(props) {
+  return (
+    <div className="border">
+    {category.map((categorys, i) => (
+      <button className="p-2 bg-gray-200 m-2" key={i}>{categorys}</button>
+    ))}
+    </div>
+  )
+}
+
+
+export default App;
