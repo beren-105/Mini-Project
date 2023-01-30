76cc8272ac5e766ecaecd9ec493d128880377b16
Beren <k33250@naver.com>
2023-01-30 14:45:23 +0900

    update all

--- 4_youtube/src/App.js
+++ 4_youtube/src/App.js
@@ -1,8 +1,18 @@
+import React, { useState } from "react"
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from "react-router-dom"
+
 import './App.css';
 import Header from './Header'
+import Popular from './Popular'
+import SideBar from './SideBar'
 
-const content = [
-  {id: 'watch-0', user: 'chster', category: '음악'},
+const contents = [
+  {id: 'content-0', user: '우디', category: '음악', title: 'Say I Love You', thumbnail: 'https://i.ytimg.com/vi/C0wHFZ_B4sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCegaTzRmTWeZ-1c_T26HP-QOlTpw', src: 'https://www.youtube.com/watch?v=C0wHFZ_B4sg'},
+  {id: 'content-1', user: 'chster', category: '실시간', thumbnail: 'https://i.ytimg.com/vi/KKalqB3CcS8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAaKCkpLN_yUn4sV8Pi6bxVoxN0pQ'},
+  {id: 'content-2', user: 'chster', category: '게임', thumbnail: 'https://i.ytimg.com/vi/VBB_Amxd2-g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBzYYgQe5RgbJxzPlUXbbm2_p5p3Q'},
+  {id: 'content-3', user: 'MBCNEWS', category: '뉴스',title: '실내 마스크 해제 첫날‥시민들 표정은?', profile: 'https://yt3.ggpht.com/ytc/AL5GRJVS7X22afAWJV6tZrLItlKR4NcVOL9qulxw6pC9Zg=s48-c-k-c0x00ffffff-no-rj', thumbnail: 'https://i.ytimg.com/vi/5f6O0xpyUwo/hq720_live.jpg?sqp=CIyv2Z4G-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBBeLQnSD9RB-at-3GXe0GwXGcGMA', src:'https://www.youtube.com/watch?v=_PzClmeY5tk'},
+  {id: 'content-4', user: 'chster', category: '축구', thumbnail: '', src: 'https://www.youtube.com/embed/8hwuKBvM2Iw'},
+  {id: 'content-5', user: 'chster', category: '요리', thumbnail: ''},
 ]
 
 const category = ['전체', '음악', '실시간', '게임', '뉴스', '축구', '요리']
@@ -12,24 +22,15 @@ function App() {
 
   return (
     <>
-      <Header />
-      <FliterBtn 
-        category = {category}
+      <Header
+      category = {category}
       />
+      <SideBar />
+      <Popular />
     </>
   )
 }
 
-
-function FliterBtn(props) {
-  return (
-    <div className="">
-    {category.map((categorys, i) => (
-      <button className="p-2 bg-gray-200 m-2" key={i}>{categorys}</button>
-    ))}
-    </div>
-  )
-}
 
 
 export default App;
---
+++ 4_youtube/src/Popular.js
@@ -0,0 +1,93 @@
+function Popular() {
+    return (
+        <section className="flex flex-wrap mt-4 max-sm:px-20 xl:ml-56">
+            <article className="px-2 mb-16 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+                <img
+                    className="w-full object-cover mb-2 rounded-xl"
+                    src="https://i.ytimg.com/vi/VBB_Amxd2-g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBzYYgQe5RgbJxzPlUXbbm2_p5p3Q"
+                />
+                <div className="flex">
+                    <div className="w-10 h-10 mr-4 rounded-full object-cover bg-gray-200"></div>
+                    <div>
+                        <h4 className="mb-1">타이틀을 여기에 적기</h4>
+                        <p className="text-xs text-zinc-500">유저이름</p>
+                        <p className="text-xs text-zinc-500">조회수 12만회 · 12시간 전</p>
+                    </div>
+                </div>
+            </article>
+            <article className="px-4 mb-16 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+                <img
+                    className="w-full object-cover mb-2"
+                    src="https://i.ytimg.com/vi/VBB_Amxd2-g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBzYYgQe5RgbJxzPlUXbbm2_p5p3Q"
+                />
+                <div className="flex">
+                    <div className="w-10 h-10 mr-4 rounded-full object-cover bg-gray-200"></div>
+                    <div>
+                        <h4 className="mb-1">타이틀을 여기에 적기</h4>
+                        <p className="text-xs text-zinc-500">유저이름</p>
+                        <p className="text-xs text-zinc-500">조회수 12만회 · 12시간 전</p>
+                    </div>
+                </div>
+            </article>
+            <article className="px-4 mb-16 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+                <img
+                    className="w-full object-cover mb-2"
+                    src="https://i.ytimg.com/vi/VBB_Amxd2-g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBzYYgQe5RgbJxzPlUXbbm2_p5p3Q"
+                />
+                <div className="flex">
+                    <div className="w-10 h-10 mr-4 rounded-full object-cover bg-gray-200"></div>
+                    <div>
+                        <h4 className="mb-1">타이틀을 여기에 적기</h4>
+                        <p className="text-xs text-zinc-500">유저이름</p>
+                        <p className="text-xs text-zinc-500">조회수 12만회 · 12시간 전</p>
+                    </div>
+                </div>
+            </article>
+            <article className="px-4 mb-16 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+                <img
+                    className="w-full object-cover mb-2"
+                    src="https://i.ytimg.com/vi/VBB_Amxd2-g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBzYYgQe5RgbJxzPlUXbbm2_p5p3Q"
+                />
+                <div className="flex">
+                    <div className="w-10 h-10 mr-4 rounded-full object-cover bg-gray-200"></div>
+                    <div>
+                        <h4 className="mb-1">타이틀을 여기에 적기</h4>
+                        <p className="text-xs text-zinc-500">유저이름</p>
+                        <p className="text-xs text-zinc-500">조회수 12만회 · 12시간 전</p>
+                    </div>
+                </div>
+            </article>
+            <article className="px-4 mb-16 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+                <img
+                    className="w-full object-cover mb-2"
+                    src="https://i.ytimg.com/vi/VBB_Amxd2-g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBzYYgQe5RgbJxzPlUXbbm2_p5p3Q"
+                />
+                <div className="flex">
+                    <div className="w-10 h-10 mr-4 rounded-full object-cover bg-gray-200"></div>
+                    <div>
+                        <h4 className="mb-1">타이틀을 여기에 적기</h4>
+                        <p className="text-xs text-zinc-500">유저이름</p>
+                        <p className="text-xs text-zinc-500">조회수 12만회 · 12시간 전</p>
+                    </div>
+                </div>
+            </article>
+            <article className="px-4 mb-16 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+                <img
+                    className="w-full object-cover mb-2"
+                    src="https://i.ytimg.com/vi/VBB_Amxd2-g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBzYYgQe5RgbJxzPlUXbbm2_p5p3Q"
+                />
+                <div className="flex">
+                    <div className="w-10 h-10 mr-4 rounded-full object-cover bg-gray-200"></div>
+                    <div>
+                        <h4 className="mb-1">타이틀을 여기에 적기</h4>
+                        <p className="text-xs text-zinc-500">유저이름</p>
+                        <p className="text-xs text-zinc-500">조회수 12만회 · 12시간 전</p>
+                    </div>
+                </div>
+            </article>
+        </section>
+        
+    )
+}
+
+export default Popular
