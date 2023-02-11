e8c7935e4256e5024c9d9b29bf344af6741b15e9
Beren <k33250@naver.com>
2023-02-11 22:22:35 +0900

    Add Calender

---
+++ 6_calender/script.js
@@ -0,0 +1,2 @@
+var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+var week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
---
+++ 6_calender/script.ts
@@ -0,0 +1,3 @@
+const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
