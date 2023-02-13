3e29d0dba816bbd90b4b137ac6c9b18ae4e8e2c8
Beren <k33250@naver.com>
2023-02-13 21:06:12 +0900

    Calender Complete

--- 6_calender/script.js
+++ 6_calender/script.js
@@ -23,9 +23,11 @@ var day = function (nowLast) {
     var tableDay = document.createElement('div');
     tableDay.classList.add('tableDay', 'grid');
     tableDays === null || tableDays === void 0 ? void 0 : tableDays.appendChild(tableDay);
+    // 월 출력하기
     if (mounthTitle instanceof HTMLHeadingElement) {
-        mounthTitle.innerText = monthName[nowLast.getMonth()];
+        mounthTitle.innerText = nowLast.getFullYear() + ', ' + monthName[nowLast.getMonth()];
     }
+    //요일 출력하기
     if (tableDay instanceof HTMLDivElement) {
         if (prevLast.getDay() !== 6) {
             for (var i = prevLast.getDate() - prevLast.getDay(); i <= prevLast.getDate(); i++) {
@@ -50,7 +52,45 @@ var day = function (nowLast) {
             }
         }
     }
+    var dayPTages = document.querySelectorAll('.now');
+    // 오늘인 날짜 활성화하기
+    if (date.getFullYear() === nowLast.getFullYear() && date.getMonth() === nowLast.getMonth()) {
+        dayPTages.forEach(function (dayPTage) {
+            if (Number(dayPTage.innerText) === nowLast.getDate()) {
+                dayPTage.classList.add('nowDay');
+            }
+        });
+    }
+    // 클릭 시 일자 활성화하기
+    dayPTages.forEach(function (dayPTage) {
+        dayPTage.addEventListener('click', pClick);
+    });
 };
+// a 날짜에서 b 날짜까지 선택하기
+var clickIndex = 0;
+var clickItem = [];
+var pClick = function (e) {
+    clickIndex++;
+    if (e.target instanceof HTMLParagraphElement) {
+        switch (clickIndex) {
+            case 1:
+                e.target.classList.add('from');
+                clickItem.push(e.target);
+                break;
+            case 2:
+                e.target.classList.add('to');
+                clickItem.push(e.target);
+                break;
+            case 3:
+                clickItem.forEach(function (dayPTage) {
+                    dayPTage.classList.remove('from', 'to');
+                });
+                clickIndex = 0;
+                break;
+        }
+    }
+};
+// 버튼 클릭 이벤트
 var prevBtn = document.querySelector('.prevBtn');
 var nextBtn = document.querySelector('.nextBtn');
 var index = 0;
@@ -60,9 +100,29 @@ prevBtn === null || prevBtn === void 0 ? void 0 : prevBtn.addEventListener('clic
     nowLast = new Date(date.getFullYear(), (date.getMonth() + 1) - index, 0);
     day(nowLast, prevLast);
     if (tableDays instanceof HTMLDivElement) {
-        tableDays.style.transform = "translateY(-".concat(260 * index, "px)");
+        tableDays.style.transition = '0.5s';
+        tableDays.style.transform = "translateX(-".concat(364, "px)");
     }
     setTimeout(remove, 500);
 });
+nextBtn === null || nextBtn === void 0 ? void 0 : nextBtn.addEventListener('click', function () {
+    index--;
+    prevLast = new Date(date.getFullYear(), date.getMonth() - index, 0);
+    nowLast = new Date(date.getFullYear(), (date.getMonth() + 1) - index, 0);
+    console.log(prevLast.getMonth());
+    day(nowLast, prevLast);
+    if (tableDays instanceof HTMLDivElement) {
+        tableDays.style.transition = '0.5s';
+        tableDays.style.transform = "translateX(-".concat(364, "px)");
+    }
+    setTimeout(remove, 500);
+    console.log(index);
+});
 function remove() {
+    var _a;
+    (_a = tableDays === null || tableDays === void 0 ? void 0 : tableDays.firstChild) === null || _a === void 0 ? void 0 : _a.remove();
+    if (tableDays instanceof HTMLDivElement) {
+        tableDays.style.transition = '0s';
+        tableDays.style.transform = "translateX(0px)";
+    }
 }
--- 6_calender/script.ts
+++ 6_calender/script.ts
@@ -1,12 +1,12 @@
 const monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
 const weekName = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-
 const date = new Date()
 let prevLast = new Date()
 let nowLast = new Date()
 
 const tableDays = document.querySelector('.tableDays')
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const tableWeek = document.querySelector('.tableWeek')
@@ -35,11 +35,12 @@ let day: Days = function (nowLast) {
     tableDay.classList.add('tableDay', 'grid')
     tableDays?.appendChild(tableDay)
 
-
+    // 월 출력하기
     if (mounthTitle instanceof HTMLHeadingElement) {
-        mounthTitle.innerText = monthName[nowLast.getMonth()]
+        mounthTitle.innerText = nowLast.getFullYear() + ', ' + monthName[nowLast.getMonth()]
     }
 
+    //요일 출력하기
     if (tableDay instanceof HTMLDivElement) {
         if (prevLast.getDay() !== 6) {
             for (let i=prevLast.getDate() - prevLast.getDay(); i<=prevLast.getDate(); i++) {
@@ -65,8 +66,57 @@ let day: Days = function (nowLast) {
             }
         }
     }
+
+    
+    const dayPTages = document.querySelectorAll('.now')
+
+    // 오늘인 날짜 활성화하기
+    if (date.getFullYear() === nowLast.getFullYear() && date.getMonth() === nowLast.getMonth()) {
+        dayPTages.forEach((dayPTage :HTMLParagraphElement) => {
+            if (Number(dayPTage.innerText) === nowLast.getDate()) {
+                dayPTage.classList.add('nowDay')
+            }
+        })
+    }
+
+    // 클릭 시 일자 활성화하기
+    dayPTages.forEach((dayPTage :HTMLParagraphElement) => {
+        dayPTage.addEventListener('click', pClick)
+    })
+    
+
 }
 
+// a 날짜에서 b 날짜까지 선택하기
+let clickIndex = 0
+let clickItem :Element[] = []
+type PClick = (e :MouseEvent) => void
+
+let pClick :PClick = function(e) {
+    
+    clickIndex++
+    if (e.target instanceof HTMLParagraphElement) {
+        switch (clickIndex) {
+            case 1 :
+                e.target.classList.add('from')
+                clickItem.push(e.target)
+                break
+            case 2 :
+                e.target.classList.add('to')
+                clickItem.push(e.target)
+                break
+            case 3 :
+                clickItem.forEach((dayPTage) => {
+                    dayPTage.classList.remove('from', 'to')
+                })
+                clickIndex=0
+                break
+        }
+        
+    }
+}
+
+// 버튼 클릭 이벤트
 const prevBtn = document.querySelector('.prevBtn')
 const nextBtn = document.querySelector('.nextBtn')
 let index = 0
@@ -78,12 +128,36 @@ prevBtn?.addEventListener('click', () => {
     day(nowLast, prevLast)
     
     if (tableDays instanceof HTMLDivElement) {
-        tableDays.style.transform = `translateY(-${260*index}px)`
+        tableDays.style.transition = '0.5s'
+        tableDays.style.transform = `translateX(-${364}px)`
     }
 
     setTimeout(remove,500)
 })
 
-function remove() {
+nextBtn?.addEventListener('click', () => {
+    index--
+    prevLast = new Date(date.getFullYear(), date.getMonth()-index, 0)
+    nowLast = new Date(date.getFullYear(), (date.getMonth()+1)-index, 0)
+    console.log(prevLast.getMonth())
+    day(nowLast, prevLast)
     
+    if (tableDays instanceof HTMLDivElement) {
+        tableDays.style.transition = '0.5s'
+        tableDays.style.transform = `translateX(-${364}px)`
+    }
+
+    setTimeout(remove,500)
+    
+    console.log(index)
+})
+
+function remove() {
+    tableDays?.firstChild?.remove()
+    if (tableDays instanceof HTMLDivElement) {
+        tableDays.style.transition = '0s'
+        tableDays.style.transform = `translateX(0px)`
+    }
 }
+
+
