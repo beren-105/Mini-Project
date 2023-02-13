9846df81d0a9e4fb865c7688b36ceb1d84b93b95
Beren <k33250@naver.com>
2023-02-13 17:30:32 +0900

    Day Update

--- 6_calender/script.js
+++ 6_calender/script.js
@@ -6,6 +6,7 @@ var nowLast = new Date();
 var tableDays = document.querySelector('.tableDays');
 document.addEventListener('DOMContentLoaded', function () {
     var tableWeek = document.querySelector('.tableWeek');
+    prevLast = new Date(date.getFullYear(), date.getMonth(), 0);
     nowLast = new Date(date.getFullYear(), date.getMonth() + 1, 0);
     // 요일 출력하기
     if (tableWeek instanceof HTMLDivElement) {
@@ -15,23 +16,24 @@ document.addEventListener('DOMContentLoaded', function () {
             tableWeek.appendChild(p);
         });
     }
-    day(nowLast);
+    day(nowLast, prevLast);
 });
 var day = function (nowLast) {
     var mounthTitle = document.querySelector('.title');
     var tableDay = document.createElement('div');
     tableDay.classList.add('tableDay', 'grid');
     tableDays === null || tableDays === void 0 ? void 0 : tableDays.appendChild(tableDay);
-    prevLast = new Date(date.getFullYear(), date.getMonth(), 0);
     if (mounthTitle instanceof HTMLHeadingElement) {
         mounthTitle.innerText = monthName[nowLast.getMonth()];
     }
     if (tableDay instanceof HTMLDivElement) {
-        for (var i = prevLast.getDate() - prevLast.getDay(); i <= prevLast.getDate(); i++) {
-            var p = document.createElement('p');
-            p.classList.add('prev');
-            p.innerHTML = i.toString();
-            tableDay.appendChild(p);
+        if (prevLast.getDay() !== 6) {
+            for (var i = prevLast.getDate() - prevLast.getDay(); i <= prevLast.getDate(); i++) {
+                var p = document.createElement('p');
+                p.classList.add('prev');
+                p.innerHTML = i.toString();
+                tableDay.appendChild(p);
+            }
         }
         for (var i = 1; i <= nowLast.getDate(); i++) {
             var p = document.createElement('p');
@@ -39,11 +41,13 @@ var day = function (nowLast) {
             p.innerText = i.toString();
             tableDay.appendChild(p);
         }
-        for (var i = 1; i <= (6 - nowLast.getDay() === 6 ? 0 : 6 - nowLast.getDay()); i++) {
-            var p = document.createElement('p');
-            p.classList.add('next');
-            p.innerText = i.toString();
-            tableDay.appendChild(p);
+        if (nowLast.getDay() !== 6) {
+            for (var i = 1; i <= (6 - nowLast.getDay() === 0 ? 1 : 6 - nowLast.getDay()); i++) {
+                var p = document.createElement('p');
+                p.classList.add('next');
+                p.innerText = i.toString();
+                tableDay.appendChild(p);
+            }
         }
     }
 };
@@ -51,11 +55,14 @@ var prevBtn = document.querySelector('.prevBtn');
 var nextBtn = document.querySelector('.nextBtn');
 var index = 0;
 prevBtn === null || prevBtn === void 0 ? void 0 : prevBtn.addEventListener('click', function () {
-    var tableDay = document.querySelector('.tableDay');
     index++;
-    nowLast = new Date(date.getFullYear(), date.getMonth() - index, 0);
-    day(nowLast);
+    prevLast = new Date(date.getFullYear(), date.getMonth() - index, 0);
+    nowLast = new Date(date.getFullYear(), (date.getMonth() + 1) - index, 0);
+    day(nowLast, prevLast);
     if (tableDays instanceof HTMLDivElement) {
         tableDays.style.transform = "translateY(-".concat(260 * index, "px)");
     }
+    setTimeout(remove, 500);
 });
+function remove() {
+}
--- 6_calender/script.ts
+++ 6_calender/script.ts
@@ -11,6 +11,7 @@ const tableDays = document.querySelector('.tableDays')
 document.addEventListener('DOMContentLoaded', () => {
     const tableWeek = document.querySelector('.tableWeek')
     
+    prevLast = new Date(date.getFullYear(), date.getMonth(), 0)
     nowLast = new Date(date.getFullYear(), date.getMonth()+1, 0)
 
     // 요일 출력하기
@@ -22,12 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    day(nowLast)
+    day(nowLast, prevLast)
 
 })
 
-type Days = (nowLast :Date) => void
-
+type Days = (nowLast :Date, prevLast :Date) => void
 let day: Days = function (nowLast) {
     const mounthTitle = document.querySelector('.title')
 
@@ -35,18 +35,19 @@ let day: Days = function (nowLast) {
     tableDay.classList.add('tableDay', 'grid')
     tableDays?.appendChild(tableDay)
 
-    prevLast = new Date(date.getFullYear(), date.getMonth(), 0)
 
     if (mounthTitle instanceof HTMLHeadingElement) {
         mounthTitle.innerText = monthName[nowLast.getMonth()]
     }
 
     if (tableDay instanceof HTMLDivElement) {
-        for (let i=prevLast.getDate() - prevLast.getDay(); i<=prevLast.getDate(); i++) {
-            const p = document.createElement('p')
-            p.classList.add('prev')
-            p.innerHTML = i.toString()
-            tableDay.appendChild(p)
+        if (prevLast.getDay() !== 6) {
+            for (let i=prevLast.getDate() - prevLast.getDay(); i<=prevLast.getDate(); i++) {
+                const p = document.createElement('p')
+                p.classList.add('prev')
+                p.innerHTML = i.toString()
+                tableDay.appendChild(p)
+            }
         }
         for (let i=1; i<=nowLast.getDate(); i++) {
             const p = document.createElement('p')
@@ -54,11 +55,14 @@ let day: Days = function (nowLast) {
             p.innerText = i.toString()
             tableDay.appendChild(p)
         }
-        for (let i=1; i<=(6 - nowLast.getDay() === 6 ? 0 : 6 - nowLast.getDay()); i++) {
-            const p = document.createElement('p')
-            p.classList.add('next')
-            p.innerText = i.toString()
-            tableDay.appendChild(p)
+        
+        if (nowLast.getDay() !== 6) {
+            for (let i=1; i<=(6 - nowLast.getDay() === 0 ? 1 : 6 - nowLast.getDay()); i++) {
+                const p = document.createElement('p')
+                p.classList.add('next')
+                p.innerText = i.toString()
+                tableDay.appendChild(p)
+            }
         }
     }
 }
@@ -68,12 +72,18 @@ const nextBtn = document.querySelector('.nextBtn')
 let index = 0
 
 prevBtn?.addEventListener('click', () => {
-    const tableDay = document.querySelector('.tableDay')
     index++
-    nowLast = new Date(date.getFullYear(), date.getMonth()-index, 0)
-    day(nowLast)
+    prevLast = new Date(date.getFullYear(), date.getMonth()-index, 0)
+    nowLast = new Date(date.getFullYear(), (date.getMonth()+1)-index, 0)
+    day(nowLast, prevLast)
     
     if (tableDays instanceof HTMLDivElement) {
         tableDays.style.transform = `translateY(-${260*index}px)`
     }
+
+    setTimeout(remove,500)
 })
+
+function remove() {
+    
+}
