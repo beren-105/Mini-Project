ce5fe84ce872e9c70453fc64b7aa1d7738e75d3d
Beren <k33250@naver.com>
2023-02-13 15:02:22 +0900

    Calender Update

--- 6_calender/script.js
+++ 6_calender/script.js
@@ -3,9 +3,9 @@ var weekName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 var date = new Date();
 var prevLast = new Date();
 var nowLast = new Date();
-console.log(prevLast.getDate());
+var tableDay = document.querySelector('.tableDay');
+var tableWeek = document.querySelector('.tableWeek');
 document.addEventListener('DOMContentLoaded', function () {
-    var tableWeek = document.querySelector('.tableWeek');
     prevLast = new Date(date.getFullYear(), date.getMonth(), 0);
     nowLast = new Date(date.getFullYear(), date.getMonth() + 1, 0);
     // 요일 출력하기
@@ -16,41 +16,61 @@ document.addEventListener('DOMContentLoaded', function () {
             tableWeek.appendChild(p);
         });
     }
-    day();
-});
-function day() {
-    var tableDay = document.querySelector('.tableDay');
-    var mounthTitle = document.querySelector('.title');
-    if (mounthTitle instanceof HTMLHeadingElement) {
-        mounthTitle.innerText = monthName[date.getMonth()];
-    }
     if (tableDay instanceof HTMLDivElement) {
         for (var i = prevLast.getDay(); i >= 0; i--) {
             var p = document.createElement('p');
             p.classList.add('prev');
-            p.innerText = (prevLast.getDate() - i).toString();
             tableDay.appendChild(p);
         }
         for (var i = 1; i <= nowLast.getDate(); i++) {
             var p = document.createElement('p');
             p.classList.add('now');
-            p.innerText = i.toString();
             tableDay.appendChild(p);
         }
         for (var i = 1; i <= (!0 ? 6 - nowLast.getDay() : 1); i++) {
             var p = document.createElement('p');
             p.classList.add('next');
-            p.innerText = i.toString();
             tableDay.appendChild(p);
-            console.log(nowLast.getDay());
         }
     }
-}
-// 일 출력하기
+    day(prevLast, nowLast);
+});
+var day = function (prevLast, nowLast) {
+    var mounthTitle = document.querySelector('.title');
+    var prev = document.querySelectorAll('.prev');
+    var now = document.querySelectorAll('.now');
+    var next = document.querySelectorAll('.next');
+    if (mounthTitle instanceof HTMLHeadingElement) {
+        mounthTitle.innerText = monthName[nowLast.getMonth()];
+    }
+    var prevLastArr = [];
+    for (var i = prevLast.getDay(); i >= 0; i--) {
+        prevLastArr.push(i);
+    }
+    prev.forEach(function (prevs, i) {
+        if (prevs instanceof HTMLParagraphElement) {
+            prevs.innerHTML = (prevLast.getDate() - (prevLastArr[i])).toString();
+        }
+    });
+    now.forEach(function (nows, i) {
+        if (nows instanceof HTMLParagraphElement) {
+            nows.innerText = (i + 1).toString();
+        }
+    });
+    for (var i = 0; i <= (!0 ? 6 - nowLast.getDay() : 1); i++) {
+        var a = next[i];
+        {
+            a instanceof HTMLParagraphElement ?
+                a.innerText = (i + 1).toString()
+                : undefined;
+        }
+    }
+};
 var prevBtn = document.querySelector('.prevBtn');
 var nextBtn = document.querySelector('.nextBtn');
 prevBtn === null || prevBtn === void 0 ? void 0 : prevBtn.addEventListener('click', function () {
-    // prevLast = new Date(date.getFullYear(), date.getMonth(), 0)
-    nowLast = new Date(date.getFullYear(), date.getMonth() - 1, 0);
-    day();
+    console.log('zzz');
+    prevLast = new Date(date.getFullYear(), date.getMonth() - 1, 0);
+    nowLast = new Date(date.getFullYear(), date.getMonth(), 0, 0);
+    day(prevLast, nowLast);
 });
--- 6_calender/script.ts
+++ 6_calender/script.ts
@@ -7,10 +7,12 @@ const date = new Date()
 let prevLast = new Date()
 let nowLast = new Date()
 
-console.log(prevLast.getDate())
+const tableDay = document.querySelector('.tableDay')
+const tableWeek = document.querySelector('.tableWeek')
+
+type Days = (prevLast :Date, nowLast :Date) => void
 
 document.addEventListener('DOMContentLoaded', () => {
-    const tableWeek = document.querySelector('.tableWeek')
     prevLast = new Date(date.getFullYear(), date.getMonth(), 0)
     nowLast = new Date(date.getFullYear(), date.getMonth()+1, 0)
 
@@ -23,46 +25,70 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    day()
-})
-
-function day() {
-    const tableDay = document.querySelector('.tableDay')
-    const mounthTitle = document.querySelector('.title')
-
-    if (mounthTitle instanceof HTMLHeadingElement) {
-        mounthTitle.innerText = monthName[date.getMonth()]
-    }
-    
     if (tableDay instanceof HTMLDivElement) {
         for (let i=prevLast.getDay(); i>=0; i--) {
             const p = document.createElement('p')
             p.classList.add('prev')
-            p.innerText = (prevLast.getDate()-i).toString()
             tableDay.appendChild(p)
         }
         for (let i=1; i<=nowLast.getDate(); i++) {
             const p = document.createElement('p')
             p.classList.add('now')
-            p.innerText = i.toString()
             tableDay.appendChild(p)
         }
         for (let i=1; i<=(!0 ? 6-nowLast.getDay() : 1); i++) {
             const p = document.createElement('p')
             p.classList.add('next')
-            p.innerText = i.toString()
             tableDay.appendChild(p)
-            console.log(nowLast.getDay())
         }
     }
+
+    day(prevLast, nowLast)
+})
+
+let day :Days = function (prevLast, nowLast) {
+    const mounthTitle = document.querySelector('.title')
+
+    const prev = document.querySelectorAll('.prev')
+    const now = document.querySelectorAll('.now')
+    const next = document.querySelectorAll('.next')
+    if (mounthTitle instanceof HTMLHeadingElement) {
+        mounthTitle.innerText = monthName[nowLast.getMonth()]
+    }
+
+    let prevLastArr : number[] = []
+    
+    for (let i=prevLast.getDay(); i>=0; i--) {
+        prevLastArr.push(i)
+    }
+    prev.forEach((prevs :HTMLParagraphElement, i :number) => {
+        if (prevs instanceof HTMLParagraphElement) {
+            prevs.innerHTML = (prevLast.getDate()-(prevLastArr[i])).toString()
+        }
+    })
+    
+    now.forEach((nows :HTMLElement, i :number) => {
+        if (nows instanceof HTMLParagraphElement) {
+            nows.innerText = (i+1).toString()
+        }
+    })
+
+    for (let i=0; i<=(!0 ? 6-nowLast.getDay() : 1); i++) {
+        const a = next[i]
+        {a instanceof HTMLParagraphElement ?
+            a.innerText = (i+1).toString()
+            : undefined
+        }
+    }
+    
 }
-// 일 출력하기
 
 const prevBtn = document.querySelector('.prevBtn')
 const nextBtn = document.querySelector('.nextBtn')
 
 prevBtn?.addEventListener('click', () => {
-    // prevLast = new Date(date.getFullYear(), date.getMonth(), 0)
-    nowLast = new Date(date.getFullYear(), date.getMonth()-1, 0)
-    day()
+    console.log('zzz')
+    prevLast = new Date(date.getFullYear(), date.getMonth()-1, 0)
+    nowLast = new Date(date.getFullYear(), date.getMonth()0, 0)
+    day(prevLast, nowLast)
 })
