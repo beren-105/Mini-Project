299ce8b11d7e099210eb23895fdf027651417cb8
Beren <k33250@naver.com>
2023-03-10 14:26:23 +0900

    created keys

--- 8_Piano/pianoItems.ts
+++ 8_Piano/pianoItems.ts
@@ -1,203 +1,180 @@
 
-export const whiteKey = [
+export const whiteKeys = [
     {
         type: 'key',
-        idVaule: 'c4',
-        keyboard: 'Q',
+        dateVaule: 'C',
+        keyboard: 'KeyQ',
         src: './piano-mp3/C4.mp3',
         syllableName: '도'
     },
     {
         type: 'key',
-        idVaule: 'd4',
-        keyboard: 'W',
+        dateVaule: 'D',
+        keyboard: 'KeyW',
         src: './piano-mp3/D4.mp3',
         syllableName: '레'
     },
     {
         type: 'key',
-        idVaule: 'e4',
-        keyboard: 'E',
+        dateVaule: 'E',
+        keyboard: 'KeyE',
         src: './piano-mp3/E4.mp3',
         syllableName: '미'
     },
     {
         type: 'key',
-        idVaule: 'f4',
-        keyboard: 'R',
+        dateVaule: 'F',
+        keyboard: 'KeyR',
         src: './piano-mp3/F4.mp3',
         syllableName: '파'
     },
     {
         type: 'key',
-        idVaule: 'g4',
-        keyboard: 'T',
+        dateVaule: 'G',
+        keyboard: 'KeyT',
         src: './piano-mp3/G4.mp3',
         syllableName: '솔'
     },
     {
         type: 'key',
-        idVaule: 'a4',
-        keyboard: 'Y',
+        dateVaule: 'A',
+        keyboard: 'KeyY',
         src: './piano-mp3/A4.mp3',
         syllableName: '라'
     },
     {
         type: 'key',
-        idVaule: 'a4',
-        keyboard: 'U',
+        dateVaule: 'B',
+        keyboard: 'KeyU',
         src: './piano-mp3/B4.mp3',
         syllableName: '시'
     },
     {
         type: 'key',
-        idVaule: 'c5',
-        keyboard: 'Z',
+        dateVaule: 'C',
+        keyboard: 'KeyZ',
         src: './piano-mp3/C5.mp3',
         syllableName: '도'
     },
     {
         type: 'key',
-        idVaule: 'd5',
-        keyboard: 'X',
+        dateVaule: 'D',
+        keyboard: 'KeyX',
         src: './piano-mp3/D5.mp3',
         syllableName: '레'
     },
     {
         type: 'key',
-        idVaule: 'e5',
-        keyboard: 'C',
+        dateVaule: 'E',
+        keyboard: 'KeyC',
         src: './piano-mp3/E5.mp3',
         syllableName: '미'
     },
     {
         type: 'key',
-        idVaule: 'f5',
-        keyboard: 'V',
+        dateVaule: 'F',
+        keyboard: 'KeyV',
         src: './piano-mp3/F4.mp3',
         syllableName: '파'
     },
     {
         type: 'key',
-        idVaule: 'g5',
-        keyboard: 'B',
+        dateVaule: 'G',
+        keyboard: 'KeyB',
         src: './piano-mp3/G5.mp3',
         syllableName: '솔'
     },
     {
         type: 'key',
-        idVaule: 'a5',
-        keyboard: 'N',
+        dateVaule: 'A',
+        keyboard: 'KeyN',
         src: './piano-mp3/A5.mp3',
         syllableName: '라'
     },
     {
         type: 'key',
-        idVaule: 'a5',
-        keyboard: 'M',
+        dateVaule: 'B',
+        keyboard: 'KeyM',
         src: './piano-mp3/B5.mp3',
         syllableName: '시'
+    },
+    {
+        type: 'key',
+        dateVaule: 'C',
+        keyboard: 'Comma',
+        src: './piano-mp3/C5.mp3',
+        syllableName: '도'
     }
 ]
 
-export const blackKey = [
+export const blackKeys = [
     {
         type: 'sharp',
-        idVaule: 'c4',
-        keyboard: 'Q',
-        src: './piano-mp3/C4.mp3',
-        syllableName: '도'
-    },
-    {
-        type: 'sharp',
-        idVaule: 'd4',
-        keyboard: 'W',
-        src: './piano-mp3/D4.mp3',
+        dateVaule: 'D♭',
+        keyboard: 'Digit2',
+        src: './piano-mp3/Db4.mp3',
         syllableName: '레'
     },
     {
         type: 'sharp',
-        idVaule: 'e4',
-        keyboard: 'E',
-        src: './piano-mp3/E4.mp3',
+        dateVaule: 'E♭',
+        keyboard: 'Digit3',
+        src: './piano-mp3/Eb4.mp3',
         syllableName: '미'
     },
     {
         type: 'sharp',
-        idVaule: 'f4',
-        keyboard: 'R',
-        src: './piano-mp3/F4.mp3',
-        syllableName: '파'
-    },
-    {
-        type: 'sharp',
-        idVaule: 'g4',
-        keyboard: 'T',
-        src: './piano-mp3/G4.mp3',
+        dateVaule: 'G♭',
+        keyboard: 'Digit5',
+        src: './piano-mp3/Gb4.mp3',
         syllableName: '솔'
     },
     {
         type: 'sharp',
-        idVaule: 'a4',
-        keyboard: 'Y',
-        src: './piano-mp3/A4.mp3',
+        dateVaule: 'A♭',
+        keyboard: 'Digit6',
+        src: './piano-mp3/Ab4.mp3',
         syllableName: '라'
     },
     {
         type: 'sharp',
-        idVaule: 'a4',
-        keyboard: 'U',
-        src: './piano-mp3/B4.mp3',
+        dateVaule: 'B♭',
+        keyboard: 'Digit7',
+        src: './piano-mp3/Bb4.mp3',
         syllableName: '시'
-    },
-    {
+    },{
         type: 'sharp',
-        idVaule: 'c5',
-        keyboard: 'Z',
-        src: './piano-mp3/C5.mp3',
-        syllableName: '도'
-    },
-    {
-        type: 'sharp',
-        idVaule: 'd5',
-        keyboard: 'X',
-        src: './piano-mp3/D5.mp3',
+        dateVaule: 'D♭',
+        keyboard: 'KeyS',
+        src: './piano-mp3/Db5.mp3',
         syllableName: '레'
     },
     {
         type: 'sharp',
-        idVaule: 'e5',
-        keyboard: 'C',
-        src: './piano-mp3/E5.mp3',
+        dateVaule: 'E♭',
+        keyboard: 'KeyD',
+        src: './piano-mp3/Eb5.mp3',
         syllableName: '미'
     },
     {
         type: 'sharp',
-        idVaule: 'f5',
-        keyboard: 'V',
-        src: './piano-mp3/F4.mp3',
-        syllableName: '파'
-    },
-    {
-        type: 'sharp',
-        idVaule: 'g5',
-        keyboard: 'B',
-        src: './piano-mp3/G5.mp3',
+        dateVaule: 'G♭',
+        keyboard: 'KeyG',
+        src: './piano-mp3/Gb5.mp3',
         syllableName: '솔'
     },
     {
         type: 'sharp',
-        idVaule: 'a5',
-        keyboard: 'N',
-        src: './piano-mp3/A5.mp3',
+        dateVaule: 'A♭',
+        keyboard: 'KeyH',
+        src: './piano-mp3/Ab5.mp3',
         syllableName: '라'
     },
     {
         type: 'sharp',
-        idVaule: 'a5',
-        keyboard: 'M',
-        src: './piano-mp3/B5.mp3',
+        dateVaule: 'B♭',
+        keyboard: 'KeyJ',
+        src: './piano-mp3/Bb5.mp3',
         syllableName: '시'
-    }
+    },
 ]
-// sharp 16개 너비
--- 8_Piano/script.js
+++ 8_Piano/script.js
@@ -1,11 +1,57 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pianoItems_js_1 = require("./pianoItems.js");
-console.log(pianoItems_js_1.whiteKey);
-console.log(pianoItems_js_1.blackKey);
-let index = 0;
-setInterval(add, 1000);
-function add() {
-    index++;
+import { whiteKeys, blackKeys } from './pianoItems.js';
+console.log(whiteKeys);
+console.log(blackKeys);
+document.addEventListener('DOMContentLoaded', pianoSetting);
+function pianoSetting() {
+    const keys = document.querySelector('.keys');
+    const sharp = document.querySelector('.sharp');
+    // 흰 건반 세팅
+    if (keys instanceof HTMLDivElement) {
+        whiteKeys.forEach((whiteKey) => {
+            const key = document.createElement('div');
+            key.classList.add('key');
+            key.setAttribute('id', 'key');
+            key.innerHTML = (`
+                <div class="key-top">
+                    <span class="key-name" >${whiteKey.dateVaule}</span>
+                </div>
+                <div class="key-bottom"></div>
+            `);
+            keys.appendChild(key);
+        });
+    }
+    // 검은 건반 세팅
+    if (sharp instanceof HTMLDivElement) {
+        blackKeys.forEach((blackKey, i) => {
+            if (i === 0 || i === 2 || i === 5 || i === 8) {
+                const empty = document.createElement('div');
+                empty.classList.add('empty');
+                sharp.appendChild(empty);
+                const sharpKey = document.createElement('div');
+                sharpKey.classList.add('sharp-key');
+                sharpKey.setAttribute('id', 'sharp-key');
+                sharpKey.innerHTML = (`
+                    <div class="sharp-inner">
+                        <span class="sharp-name">${blackKey.dateVaule}</span>
+                    </div>
+                `);
+                sharp.appendChild(sharpKey);
+            }
+            else {
+                const sharpKey = document.createElement('div');
+                sharpKey.classList.add('sharp-key');
+                sharpKey.innerHTML = (`
+                    <div class="sharp-inner">
+                        <span class="sharp-name">${blackKey.dateVaule}</span>
+                    </div>
+                `);
+                sharp.appendChild(sharpKey);
+            }
+        });
+    }
+    // 키보드 이벤트
+    document.addEventListener('keydown', (e) => playPiano(e));
 }
-console.log(index);
+function playPiano(e) {
+    console.log(e.code);
+}
