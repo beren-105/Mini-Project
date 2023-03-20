182f7a6f9598fe89086dbb0f1a8f949469443734
Beren <k33250@naver.com>
2023-03-20 15:29:49 +0900

    mouse event

--- 9_Carousel/script.js
+++ 9_Carousel/script.js
@@ -1,47 +1,74 @@
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
+const carousel = document.querySelector('.carousel');
 const slide = document.querySelector('.imgs');
 const imgs = document.querySelectorAll('.img');
 // clientWidth : 패딩값과 너비값을 포함한 값으로, 마진값을 나타나지 않는다.
 const size = imgs[0].clientWidth;
+const startPosition = size * imgs.length;
 let index = 0;
+let autoSlide;
 (function cloneSlide() {
     // 타입 어설션(type assertion)를 통한 강제 타입 교체
     // cloneNode : 복제된 node를 반환한다. 사용법 -> 기존노드.cloneNode(복제할노드)
-    const firstImgClone = imgs[0].cloneNode(true);
-    const lastImgClone = imgs[imgs.length - 1].cloneNode(true);
-    firstImgClone.setAttribute('id', 'firstImg');
-    lastImgClone.setAttribute('id', 'lastImg');
     if (slide instanceof HTMLDivElement) {
-        slide.appendChild(firstImgClone);
-        slide.prepend(lastImgClone);
-        slide.style.transform = `translateX(-${size}px)`;
-        slide.style.transition = '0.4s';
+        for (let i = 0; i < imgs.length; i++) {
+            const imgClone = imgs[i].cloneNode(true);
+            imgClone.setAttribute('id', 'clone');
+            slide.appendChild(imgClone);
+        }
+        for (let i = imgs.length - 1; i >= 0; i--) {
+            const imgClone = imgs[i].cloneNode(true);
+            imgClone.setAttribute('id', 'clone');
+            slide.prepend(imgClone);
+        }
+        slide.style.transform = `translateX(-${startPosition}px)`;
     }
 })();
-setInterval(autoSlide, 1000);
-function autoSlide() {
-    index++;
-    if (slide instanceof HTMLDivElement) {
-        slide.style.transition = '0.4s';
-        slide.style.transform = `translateX(-${index * size}px)`;
-    }
-    // console.log(index)
+if (slide instanceof HTMLDivElement
+    && carousel instanceof HTMLDivElement
+    && prevBtn instanceof HTMLButtonElement
+    && nextBtn instanceof HTMLButtonElement) {
+    autoSlide = setInterval(nextSlide, 2000);
+    slide.addEventListener('transitionend', resetSlide);
+    carousel.addEventListener('mouseover', handleMouseOver);
+    carousel.addEventListener('mouseleave', handleMouseLeave);
+    prevBtn.addEventListener('click', prevSlide);
+    nextBtn.addEventListener('click', nextSlide);
 }
-slide === null || slide === void 0 ? void 0 : slide.addEventListener('transitionend', moveSlide);
-function moveSlide() {
+function nextSlide() {
+    if (slide instanceof HTMLDivElement && index < imgs.length) {
+        index++;
+        slide.style.transition = '0.4s';
+        slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+    }
+}
+function prevSlide() {
+    if (slide instanceof HTMLDivElement && index >= 0) {
+        index--;
+        slide.style.transition = '0.4s';
+        slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+    }
+}
+function resetSlide() {
     if (slide instanceof HTMLDivElement) {
-        // if (imgs[index].id === 'lastImg') {
-        //     console.log('b');
-        //     slide.style.transition = 'none';
-        //     index = 0;
-        //     slide.style.transform = `translateX(-${index * size}px)`
-        // }
         if (index === imgs.length) {
-            console.log('a');
             slide.style.transition = 'none';
             index = 0;
-            slide.style.transform = `translateX(-${index * size}px)`;
+            slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+        }
+        if (index < 0) {
+            slide.style.transition = 'none';
+            index = imgs.length - 1;
+            slide.style.transform = `translateX(-${index * size + startPosition}px)`;
         }
     }
+}
+function handleMouseOver(e) {
+    if (e.currentTarget)
+        clearInterval(autoSlide);
+}
+function handleMouseLeave(e) {
+    if (e.currentTarget)
+        autoSlide = setInterval(nextSlide, 2000);
 }
--- 9_Carousel/script.ts
+++ 9_Carousel/script.ts
@@ -1,61 +1,89 @@
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
+const carousel = document.querySelector('.carousel');
 const slide = document.querySelector('.imgs');
 const imgs = document.querySelectorAll('.img');
 
 // clientWidth : 패딩값과 너비값을 포함한 값으로, 마진값을 나타나지 않는다.
 const size = imgs[0].clientWidth;
+const startPosition = size*imgs.length;
 let index = 0;
+let autoSlide :number;
 
 (function cloneSlide() {
     // 타입 어설션(type assertion)를 통한 강제 타입 교체
     // cloneNode : 복제된 node를 반환한다. 사용법 -> 기존노드.cloneNode(복제할노드)
-    const firstImgClone = imgs[0].cloneNode(true) as HTMLImageElement;
-    const lastImgClone = imgs[imgs.length-1].cloneNode(true) as HTMLImageElement;
-    firstImgClone.setAttribute('id', 'firstImg');
-    lastImgClone.setAttribute('id', 'lastImg');
     
     if (slide instanceof HTMLDivElement) {
-        slide.appendChild(firstImgClone);
-        slide.prepend(lastImgClone);
-        slide.style.transform = `translateX(-${size}px)`
-        slide.style.transition = '0.4s';
+        for (let i = 0; i < imgs.length; i ++) {
+            const imgClone = imgs[i].cloneNode(true) as HTMLImageElement;
+            imgClone.setAttribute('id', 'clone')
+            slide.appendChild(imgClone);
+        }
+
+        for (let i = imgs.length - 1; i >= 0 ; i --) {
+            const imgClone = imgs[i].cloneNode(true) as HTMLImageElement;
+            imgClone.setAttribute('id', 'clone')
+            slide.prepend(imgClone);
+        }
+        slide.style.transform = `translateX(-${startPosition}px)`;
     }
 })();
 
 
-setInterval(autoSlide, 1000);
 
-function autoSlide() {
-
-    index++;
-
-    if (slide instanceof HTMLDivElement) {
-        slide.style.transition = '0.4s';
-        slide.style.transform = `translateX(-${index * size}px)`;
-    }
-
-    // console.log(index)
-    
+if (slide instanceof HTMLDivElement
+    && carousel instanceof HTMLDivElement
+    && prevBtn instanceof HTMLButtonElement
+    && nextBtn instanceof HTMLButtonElement
+    ) {
+    autoSlide = setInterval(nextSlide, 2000);
+    slide.addEventListener('transitionend', resetSlide);
+    carousel.addEventListener('mouseover', handleMouseOver);
+    carousel.addEventListener('mouseleave', handleMouseLeave);
+    prevBtn.addEventListener('click', prevSlide);
+    nextBtn.addEventListener('click', nextSlide);
 }
 
+function nextSlide() {
+    if (slide instanceof HTMLDivElement && index < imgs.length) {
+        index++;
 
-slide?.addEventListener('transitionend', moveSlide);
-function moveSlide() {
+        slide.style.transition = '0.4s';
+        slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+    }
+}
 
+function prevSlide() {
+    if (slide instanceof HTMLDivElement && index >= 0) {
+        index--;
+
+        slide.style.transition = '0.4s';
+        slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+    }
+}
+
+function resetSlide() {
     if (slide instanceof HTMLDivElement) {
-        // if (imgs[index].id === 'lastImg') {
-        //     console.log('b');
-        //     slide.style.transition = 'none';
-        //     index = 0;
-        //     slide.style.transform = `translateX(-${index * size}px)`
-        // }
+        
         if (index === imgs.length) {
-            console.log('a')
             slide.style.transition = 'none';
             index = 0;
-            slide.style.transform = `translateX(-${index * size}px)`
+            slide.style.transform = `translateX(-${index * size + startPosition}px)`
+        }
+
+        if (index < 0) {
+            slide.style.transition = 'none';
+            index = imgs.length-1;
+            slide.style.transform = `translateX(-${index * size + startPosition}px)`
         }
     }
 }
 
+function handleMouseOver(e :MouseEvent) {
+    if (e.currentTarget) clearInterval(autoSlide);
+}
+
+function handleMouseLeave(e :MouseEvent) {
+    if (e.currentTarget) autoSlide = setInterval(nextSlide, 2000);
+}
