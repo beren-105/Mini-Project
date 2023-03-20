2e25a42502c0101a21afbcaa1528d3365a02e2f3
Beren <k33250@naver.com>
2023-03-20 17:02:25 +0900

    Carousel Completion

--- 9_Carousel/script.js
+++ 9_Carousel/script.js
@@ -8,35 +8,7 @@ const size = imgs[0].clientWidth;
 const startPosition = size * imgs.length;
 let index = 0;
 let autoSlide;
-document.addEventListener('DOMContentLoaded', createBtn);
-function createBtn() {
-    const slideBtns = document.querySelector('.slide-btns');
-    for (let i = 0; i < imgs.length; i++) {
-        const btn = document.createElement('button');
-        btn.classList.add('slide-btn');
-        btn.setAttribute('data-id', `${i}`);
-        if (slideBtns instanceof HTMLDivElement) {
-            slideBtns.appendChild(btn);
-        }
-        btn.addEventListener('click', dotBtnActive);
-    }
-}
-function dotBtnActive({ target }) {
-    const slideBtns = document.querySelectorAll('.slide-btn');
-    slideBtns.forEach((btn) => {
-        if (btn instanceof HTMLButtonElement) {
-            btn.classList.remove('active');
-            if (slide instanceof HTMLDivElement && target) {
-                const targetBtn = target;
-                index = Number(targetBtn.dataset.id);
-                targetBtn.classList.add('active');
-                slide.style.transition = '0.4s';
-                slide.style.transform = `translateX(-${index * size + startPosition}px)`;
-            }
-        }
-    });
-    console.log(index);
-}
+// 처음 시작할 때 클론 만들기
 (function cloneSlide() {
     // 타입 어설션(type assertion)를 통한 강제 타입 교체
     // cloneNode : 복제된 node를 반환한다. 사용법 -> 기존노드.cloneNode(복제할노드)
@@ -54,31 +26,44 @@ function dotBtnActive({ target }) {
         slide.style.transform = `translateX(-${startPosition}px)`;
     }
 })();
+// 이벤트 생성
+document.addEventListener('DOMContentLoaded', createBtn);
+autoSlide = setInterval(nextSlide, 2000);
 if (slide instanceof HTMLDivElement
     && carousel instanceof HTMLDivElement
     && prevBtn instanceof HTMLButtonElement
     && nextBtn instanceof HTMLButtonElement) {
-    autoSlide = setInterval(nextSlide, 2000);
     prevBtn.addEventListener('click', prevSlide);
     nextBtn.addEventListener('click', nextSlide);
     slide.addEventListener('transitionend', resetSlide);
     carousel.addEventListener('mouseover', handleMouseOver);
     carousel.addEventListener('mouseleave', handleMouseLeave);
 }
+// 다음으로 넘어갈 때
 function nextSlide() {
     if (slide instanceof HTMLDivElement && index < imgs.length) {
         index++;
         slide.style.transition = '0.4s';
         slide.style.transform = `translateX(-${index * size + startPosition}px)`;
     }
+    if (index === imgs.length) {
+        index = 0;
+    }
+    ;
+    dotBtnActive();
 }
+// 이전으로 넘어갈 때
 function prevSlide() {
+    if (index === 0)
+        index = imgs.length;
     if (slide instanceof HTMLDivElement && index >= 0) {
         index--;
         slide.style.transition = '0.4s';
         slide.style.transform = `translateX(-${index * size + startPosition}px)`;
     }
+    dotBtnActive();
 }
+// 무한으로 이어지는 것처럼 보이게 하게
 function resetSlide() {
     if (slide instanceof HTMLDivElement) {
         if (index === imgs.length) {
@@ -93,6 +78,7 @@ function resetSlide() {
         }
     }
 }
+// 마우스 이벤트
 function handleMouseOver(e) {
     if (e.currentTarget)
         clearInterval(autoSlide);
@@ -100,4 +86,48 @@ function handleMouseOver(e) {
 function handleMouseLeave(e) {
     if (e.currentTarget)
         autoSlide = setInterval(nextSlide, 2000);
+}
+// 하단 도트버튼 만들기
+function createBtn() {
+    const slideBtns = document.querySelector('.slide-btns');
+    for (let i = 0; i < imgs.length; i++) {
+        const btn = document.createElement('button');
+        btn.classList.add('slide-btn');
+        btn.setAttribute('data-id', `${i}`);
+        if (slideBtns instanceof HTMLDivElement) {
+            slideBtns.appendChild(btn);
+        }
+        btn.addEventListener('click', handleDotBtn);
+        dotBtnActive();
+    }
+}
+function handleDotBtn({ target }) {
+    const slideBtns = document.querySelectorAll('.slide-btn');
+    slideBtns.forEach((btn) => {
+        if (btn instanceof HTMLButtonElement) {
+            btn.classList.remove('active');
+            if (slide instanceof HTMLDivElement && target) {
+                const targetBtn = target;
+                index = Number(targetBtn.dataset.id);
+                targetBtn.classList.add('active');
+                slide.style.transition = '0.4s';
+                slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+            }
+        }
+    });
+}
+function dotBtnActive() {
+    const slideBtns = document.querySelectorAll('.slide-btn');
+    slideBtns.forEach((btn) => {
+        if (btn instanceof HTMLButtonElement) {
+            btn.classList.remove('active');
+            if (slide instanceof HTMLDivElement
+                && Number(btn.dataset.id) === index) {
+                index = Number(btn.dataset.id);
+                btn.classList.add('active');
+                slide.style.transition = '0.4s';
+                slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+            }
+        }
+    });
 }
--- 9_Carousel/script.ts
+++ 9_Carousel/script.ts
@@ -10,43 +10,8 @@ const startPosition = size*imgs.length;
 let index = 0;
 let autoSlide :number;
 
-document.addEventListener('DOMContentLoaded', createBtn)
 
-function createBtn() {
-    const slideBtns = document.querySelector('.slide-btns');
-
-    for (let i = 0; i < imgs.length; i ++) {
-        const btn = document.createElement('button');
-        btn.classList.add('slide-btn');
-        btn.setAttribute('data-id', `${i}`)
-
-        if (slideBtns instanceof HTMLDivElement) {
-            slideBtns.appendChild(btn);
-        }
-        
-        btn.addEventListener('click', dotBtnActive)
-    }
-}
-
-function dotBtnActive({ target }:FocusEvent) {
-    const slideBtns = document.querySelectorAll('.slide-btn')
-
-    slideBtns.forEach((btn) => {
-        if (btn instanceof HTMLButtonElement) {
-            btn.classList.remove('active')
-
-            if (slide instanceof HTMLDivElement && target) {
-                const targetBtn = target as HTMLButtonElement;
-                index = Number(targetBtn.dataset.id);
-                targetBtn.classList.add('active')
-                slide.style.transition = '0.4s';
-                slide.style.transform = `translateX(-${index * size + startPosition}px)`;
-            }
-        }
-    })
-    console.log(index)
-}
-
+// 처음 시작할 때 클론 만들기
 (function cloneSlide() {
     // 타입 어설션(type assertion)를 통한 강제 타입 교체
     // cloneNode : 복제된 node를 반환한다. 사용법 -> 기존노드.cloneNode(복제할노드)
@@ -68,13 +33,15 @@ function dotBtnActive({ target }:FocusEvent) {
 })();
 
 
+// 이벤트 생성
+document.addEventListener('DOMContentLoaded', createBtn);
+autoSlide = setInterval(nextSlide, 2000);
 
 if (slide instanceof HTMLDivElement
     && carousel instanceof HTMLDivElement
     && prevBtn instanceof HTMLButtonElement
     && nextBtn instanceof HTMLButtonElement
     ) {
-    autoSlide = setInterval(nextSlide, 2000);
     prevBtn.addEventListener('click', prevSlide);
     nextBtn.addEventListener('click', nextSlide);
     slide.addEventListener('transitionend', resetSlide);
@@ -82,6 +49,8 @@ if (slide instanceof HTMLDivElement
     carousel.addEventListener('mouseleave', handleMouseLeave);
 }
 
+
+// 다음으로 넘어갈 때
 function nextSlide() {
     if (slide instanceof HTMLDivElement && index < imgs.length) {
         index++;
@@ -89,17 +58,27 @@ function nextSlide() {
         slide.style.transition = '0.4s';
         slide.style.transform = `translateX(-${index * size + startPosition}px)`;
     }
+    
+    if (index === imgs.length) {index = 0};
+    dotBtnActive()
 }
 
+
+// 이전으로 넘어갈 때
 function prevSlide() {
+    if (index === 0) index = imgs.length;
+
     if (slide instanceof HTMLDivElement && index >= 0) {
         index--;
 
         slide.style.transition = '0.4s';
         slide.style.transform = `translateX(-${index * size + startPosition}px)`;
     }
+    dotBtnActive()
 }
 
+
+// 무한으로 이어지는 것처럼 보이게 하게
 function resetSlide() {
     if (slide instanceof HTMLDivElement) {
         
@@ -117,10 +96,68 @@ function resetSlide() {
     }
 }
 
+
+// 마우스 이벤트
 function handleMouseOver(e :MouseEvent) {
     if (e.currentTarget) clearInterval(autoSlide);
 }
 
 function handleMouseLeave(e :MouseEvent) {
     if (e.currentTarget) autoSlide = setInterval(nextSlide, 2000);
+}
+
+
+// 하단 도트버튼 만들기
+function createBtn() {
+    const slideBtns = document.querySelector('.slide-btns');
+
+    for (let i = 0; i < imgs.length; i ++) {
+        const btn = document.createElement('button');
+        btn.classList.add('slide-btn');
+        btn.setAttribute('data-id', `${i}`)
+
+        if (slideBtns instanceof HTMLDivElement) {
+            slideBtns.appendChild(btn);
+        }
+        
+        btn.addEventListener('click', handleDotBtn);
+        dotBtnActive()
+    }
+}
+
+function handleDotBtn({ target }:FocusEvent) {
+    const slideBtns = document.querySelectorAll('.slide-btn')
+
+    slideBtns.forEach((btn) => {
+        if (btn instanceof HTMLButtonElement) {
+            btn.classList.remove('active')
+
+            if (slide instanceof HTMLDivElement && target) {
+                const targetBtn = target as HTMLButtonElement;
+                index = Number(targetBtn.dataset.id);
+                targetBtn.classList.add('active');
+                slide.style.transition = '0.4s';
+                slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+            }
+        }
+    })
+}
+
+function dotBtnActive() {
+    const slideBtns = document.querySelectorAll('.slide-btn')
+
+    slideBtns.forEach((btn) => {
+        if (btn instanceof HTMLButtonElement) {
+            btn.classList.remove('active')
+
+            if (slide instanceof HTMLDivElement
+                && Number(btn.dataset.id) === index
+                ) {
+                index = Number(btn.dataset.id);
+                btn.classList.add('active');
+                slide.style.transition = '0.4s';
+                slide.style.transform = `translateX(-${index * size + startPosition}px)`;
+            }
+        }
+    })
 }
