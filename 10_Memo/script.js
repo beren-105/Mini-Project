5c1a624dee96f0be304f3e013f317f744b06031a
Beren <k33250@naver.com>
2023-05-04 20:46:55 +0900

    Memo update addBtn

--- 10_Memo/script.js
+++ 10_Memo/script.js
@@ -1,9 +1,40 @@
 const plusBtn = document.querySelector('.plus-btn');
 const closeBtn = document.querySelector('.close');
+const form = document.querySelector('.form');
 let memos = [
-    '만들 기능',
-    '삭제버튼, 추가버튼, 드래그 기능'
+    {
+        content: '장보기',
+        id: 0,
+    },
+    {
+        content: '운동하기',
+        id: 1,
+    },
+    {
+        content: '공부하기',
+        id: 2,
+    },
 ];
+// 첫로딩 시 화면에 메모 생성
+document.addEventListener('DOMContentLoaded', loaded);
+function loaded() {
+    const contents = document.querySelector('.contents');
+    if (contents instanceof HTMLElement) {
+        memos.forEach((memo) => {
+            const article = document.createElement('article');
+            article.classList.add('content');
+            article.setAttribute('id', `${memo.id}`);
+            article.innerHTML = `
+                <div class="btns">
+                    <button class="btn delete" type="button">Delete</button>
+                </div>
+                <textarea name="" id="" cols="30" rows="7" class="article-content">${memo.content}</textarea>
+                `;
+            contents.prepend(article);
+        });
+    }
+}
+// 모달 열기
 if (plusBtn instanceof HTMLButtonElement) {
     plusBtn.addEventListener('click', (e) => plusBtnClick(e));
     function plusBtnClick(e) {
@@ -13,6 +44,7 @@ if (plusBtn instanceof HTMLButtonElement) {
         }
     }
 }
+// 모달 닫기
 if (closeBtn instanceof HTMLButtonElement) {
     closeBtn.addEventListener('click', (e) => closeBtnClick(e));
     function closeBtnClick(e) {
@@ -22,8 +54,18 @@ if (closeBtn instanceof HTMLButtonElement) {
         }
     }
 }
-document.addEventListener('DOMContentLoaded', loaded);
-function loaded() {
-    memos.forEach((memo) => {
-    });
+// 폼 제출
+if (form instanceof HTMLFormElement) {
+    form.addEventListener('submit', (e) => handleSubmit(e));
+    function handleSubmit(e) {
+        e.preventDefault();
+        const addContent = document.querySelector('.add-content');
+        if (addContent instanceof HTMLTextAreaElement) {
+            memos = [...memos, { content: addContent.value, id: Math.floor(Math.random() * 10000) }];
+            addContent.value = '';
+            if (closeBtn instanceof HTMLButtonElement) {
+                closeBtn.click();
+            }
+        }
+    }
 }
--- 10_Memo/script.ts
+++ 10_Memo/script.ts
@@ -1,11 +1,47 @@
 
 const plusBtn = document.querySelector('.plus-btn');
 const closeBtn = document.querySelector('.close');
+const form = document.querySelector('.form');
+
 let memos = [
-    '만들 기능',
-    '삭제버튼, 추가버튼, 드래그 기능'
+    {
+        content: '장보기',
+        id: 0,
+    },
+    {
+        content: '운동하기',
+        id: 1,
+    },
+    {
+        content: '공부하기',
+        id: 2,
+    },
 ];
 
+// 첫로딩 시 화면에 메모 생성
+document.addEventListener('DOMContentLoaded', loaded);
+
+function loaded() {
+    const contents = document.querySelector('.contents');
+
+    if (contents instanceof HTMLElement) {
+        memos.forEach((memo) => {
+            const article = document.createElement('article');
+            article.classList.add('content');
+            article.setAttribute('id', `${memo.id}`);
+            article.innerHTML = `
+                <div class="btns">
+                    <button class="btn delete" type="button">Delete</button>
+                </div>
+                <textarea name="" id="" cols="30" rows="7" class="article-content">${memo.content}</textarea>
+                `;
+            contents.prepend(article);
+        });
+
+    }
+}
+
+// 모달 열기
 if (plusBtn instanceof HTMLButtonElement) {
     plusBtn.addEventListener('click',(e) => plusBtnClick(e));
 
@@ -18,6 +54,7 @@ if (plusBtn instanceof HTMLButtonElement) {
     }
 }
 
+// 모달 닫기
 if (closeBtn instanceof HTMLButtonElement) {
     closeBtn.addEventListener('click', (e) => closeBtnClick(e));
 
@@ -30,11 +67,22 @@ if (closeBtn instanceof HTMLButtonElement) {
     }
 }
 
+// 폼 제출
+if (form instanceof HTMLFormElement) {
+    form.addEventListener('submit', (e) => handleSubmit(e));
 
-document.addEventListener('DOMContentLoaded', loaded);
+    function handleSubmit(e :SubmitEvent) {
+        e.preventDefault();
 
-function loaded() {
-    memos.forEach((memo) => {
+        const addContent = document.querySelector('.add-content'); 
 
-    });
+        if (addContent instanceof HTMLTextAreaElement) {
+            memos = [...memos, {content: addContent.value, id:Math.floor(Math.random()*10000)}];
+            addContent.value = '';
+
+            if (closeBtn instanceof HTMLButtonElement) {
+                closeBtn.click();
+            }
+        }
+    }
 }
